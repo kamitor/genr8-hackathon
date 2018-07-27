bc53dbd42ba8f4e0229b65c832a28f1a31b31f7b
liamzebedee <liamzebedee@yahoo.com.au>
2018-07-27 15:04:56 +0200

    2

--- 2-run.sh
+++ 2-run.sh
@@ -1,12 +1,7 @@
+# To run on the 1st node
+# geth --datadir $(pwd)/ --networkid 420 --port 30303 --rpc --rpcport 8545 --rpccorsdomain "*" --ws --wsport 8546 --wsorigins "*" --unlock "0" --password ''  console --mine --etherbase "0"
 
-geth --datadir `pwd`/ --networkid 420 --shh  --mine --etherbase=0x0000000000000000000000000000000000000001 console
-
+# To run on joining nodes
+geth --datadir $(pwd)/ --networkid 420 --port 30304 --rpc --rpcport 8547 --rpccorsdomain "*" --ws --wsport 8548 --wsorigins "*" --password ''  console --mine --etherbase "0" --bootnodes "enode://80ade648ff3ac1a2cbc0842ae48cf80f110ea6a1600c7fdf7f588110fb659087bc89851cfc592bb1354e077769287793893f2d690278640f276fa191dadf24a2@10.209.20.155:30303"
 
 # --verbosity 9 
-#  --mine --etherbase=0x0000000000000000000000000000000000000001
-
-geth --datadir `pwd`/  --syncmode 'full' --bootnodes "enode://dbf3f60f49f22455de3830ca946dcad9d22c9a80514a345e7b7ab6a3298d7aeb642141de7c2971baf76fa29b9e27c0ac6104a0b0ffde991100bf657587c5e94a@10.209.10.212:30303" --shh console
-
-# geth --datadir `pwd`/ --networkid 420 --bootnodes "enode://10b11a11c1713c77ebf857e9279778ec7e159df21b15e9e0dbcac864a51654a5253db742a62e2e68625a4ff0bc30fb9ce25318a64cb0ca09057773076f6364e6@10.209.20.155:30303" --mine --etherbase=0x0000000000000000000000000000000000000001 --shh console
-
-
